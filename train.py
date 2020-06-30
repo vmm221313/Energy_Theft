ef16eb5acd8e541a431d52c1533b0ac59c27c98d
Varun Madhavan <vmm221313@gmail.com>
2020-06-30 16:46:27 +0530

    Added ResNet for classification

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
-from classifiers.fcn import Classifier_FCN
+#from classifiers.fcn import Classifier_FCN
+from classifiers.resnet import Classifier_RESNET
 
 with open('data/train_splits_undersampled.npz', 'rb') as load_file:
     npzfile = np.load(load_file)    
@@ -18,6 +19,8 @@ outputs_dir = 'results/fcn/'
 os.makedirs('results/', exist_ok=True)
 os.makedirs(outputs_dir, exist_ok=True)
 
-fcn = Classifier_FCN(output_directory=outputs_dir, input_shape=X_train.shape[1:], nb_classes=2, verbose=True)
+#fcn = Classifier_FCN(output_directory=outputs_dir, input_shape=X_train.shape[1:], nb_classes=2, verbose=True)
+#fcn.fit(X_train, y_train, X_val, y_val)
 
-fcn.fit(X_train, y_train, X_val, y_val)
+resnet = Classifier_RESNET(output_directory=outputs_dir, input_shape=X_train.shape[1:], nb_classes=2, verbose=True)
+resnet.fit(X_train, y_train, X_val, y_val)
