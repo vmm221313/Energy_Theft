74fa6c27bb13247f57317a5baeb9056a5a182adc
Varun Madhavan <vmm221313@gmail.com>
2020-07-02 00:33:37 +0530

    Added LSTMFCN classifier

---
+++ classifiers/lstmfcn.py
@@ -0,0 +1,66 @@
+import tensorflow.keras as keras
+import tensorflow as tf
+import numpy as np
+import time 
+
+from classifiers.base import base_Model
+
+tf.random.set_seed(42)
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+class Classifier_LSTMFCN(base_Model):
+	def __init__(self, output_directory, input_shape, nb_classes, verbose=False, build=True):
+		super(Classifier_LSTMFCN, self).__init__()
+
+		self.output_directory = output_directory
+		
+		if build == True:
+			self.model = self.build_model(input_shape, nb_classes)
+
+			if(verbose==True):
+				self.model.summary()
+
+		self.verbose = verbose
+		
+		file_path = self.output_directory+'best_model.hdf5'
+		model_checkpoint = keras.callbacks.ModelCheckpoint(filepath=file_path, monitor='val_accuracy', save_best_only=True)
+		self.callbacks.append(model_checkpoint)
+
+	def build_model(self, input_shape, nb_classes):
+		input_layer = keras.layers.Input(input_shape)
+
+		lstm = keras.layers.LSTM(8)(input_layer)
+		lstm = keras.layers.Dropout(0.2)(lstm)
+
+		permute = keras.layers.Permute((2, 1))(input_layer)
+		conv1 = keras.layers.Conv1D(filters=128, kernel_size=8, padding='same')(permute)
+		conv1 = keras.layers.BatchNormalization()(conv1)
+		conv1 = keras.layers.Activation(activation='relu')(conv1)
+
+		conv2 = keras.layers.Conv1D(filters=256, kernel_size=5, padding='same')(conv1)
+		conv2 = keras.layers.BatchNormalization()(conv2)
+		conv2 = keras.layers.Activation('relu')(conv2)
+
+		conv3 = keras.layers.Conv1D(128, kernel_size=3,padding='same')(conv2)
+		conv3 = keras.layers.BatchNormalization()(conv3)
+		conv3 = keras.layers.Activation('relu')(conv3)
+
+		gap_layer = keras.layers.GlobalAveragePooling1D()(conv3)
+
+		concat = keras.layers.concatenate([lstm, gap_layer])
+
+		output_layer = keras.layers.Dense(nb_classes, activation='softmax')(concat)
+
+		model = keras.models.Model(inputs=input_layer, outputs=output_layer)
+
+		optimizer = keras.optimizers.Adam(lr = 0.01)
+
+		model.compile(loss='categorical_crossentropy', optimizer = optimizer, metrics=['accuracy'])
+
+		session.close()
+		return model 
--- train_evaluate.py
+++ train_evaluate.py
@@ -2,51 +2,52 @@ import os
 import argparse
 import numpy as np
 from utils import save_results
-#from classifiers.fcn import Classifier_FCN
-#from classifiers.resnet import Classifier_RESNET
-'''
-
-
-model_name = 'resnet'
-
-outputs_dir = 'results/{}/'.format(model_name)
-
-
-
-classifier = Classifier_RESNET(outputs_dir=outputs_dir, input_shape=X_train.shape[1:], nb_classes=2, verbose=True)
-
-'''
-#resnet = Classifier_RESNET(outputs_dir=outputs_dir, input_shape=X_train.shape[1:], nb_classes=2, verbose=True)
-#resnet.fit(X_train, y_train, X_val, y_val)
 
 def init_classifier(classifier_name, outputs_dir, input_shape, nb_classes=2, verbose=False):
     if classifier_name == 'fcn':
         from classifiers import fcn
         return fcn.Classifier_FCN(outputs_dir, input_shape, nb_classes, verbose)
+
+    if classifier_name == 'alstmfcn':
+        from classifiers import alstmfcn
+        return alstmfcn.Classifier_ALSTMFCN(outputs_dir, input_shape, nb_classes, verbose)
+
+    if classifier_name == 'lstmfcn':
+        from classifiers import lstmfcn
+        return lstmfcn.Classifier_LSTMFCN(outputs_dir, input_shape, nb_classes, verbose)
+
     if classifier_name == 'mlp':
         from classifiers import mlp
         return mlp.Classifier_MLP(outputs_dir, input_shape, nb_classes, verbose)
+
     if classifier_name == 'resnet':
         from classifiers import resnet
         return resnet.Classifier_RESNET(outputs_dir, input_shape, nb_classes, verbose)
+
     if classifier_name == 'mcnn':
         from classifiers import mcnn
         return mcnn.Classifier_MCNN(outputs_dir, verbose)
+
     if classifier_name == 'tlenet':
         from classifiers import tlenet
         return tlenet.Classifier_TLENET(outputs_dir, verbose)
+
     if classifier_name == 'twiesn':
         from classifiers import twiesn
         return twiesn.Classifier_TWIESN(outputs_dir, verbose)
+
     if classifier_name == 'encoder':
         from classifiers import encoder
         return encoder.Classifier_ENCODER(outputs_dir, input_shape, nb_classes, verbose)
+
     if classifier_name == 'mcdcnn':
         from classifiers import mcdcnn
         return mcdcnn.Classifier_MCDCNN(outputs_dir, input_shape, nb_classes, verbose)
+
     if classifier_name == 'cnn':  # Time-CNN
         from classifiers import cnn
         return cnn.Classifier_CNN(outputs_dir, input_shape, nb_classes, verbose)
+
     if classifier_name == 'inception':
         from classifiers import inception
         return inception.Classifier_INCEPTION(outputs_dir, input_shape, nb_classes, verbose)
